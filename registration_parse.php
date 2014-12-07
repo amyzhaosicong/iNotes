452e98558d1add505d593c6ed30a570884114177
Taoran Li <litr965@gmail.com>
2014-12-07 16:53:42 -0500

    version using bootstrap

--- registration_parse.php
+++ registration_parse.php
@@ -2,17 +2,14 @@
 
 $uname = $_POST['username'];
 $pass = $_POST['password'];
+$email = $_POST['email'];
 
 include_once("connect.php");
-$sql = "SELECT * FROM users WHERE username='".$uname."' AND password='".$pass."'";
+$sql = "INSERT INTO users (username, password, email) VALUES ('".$uname."', '".$pass."', '".$email."')";
 
 $res = mysql_query($sql) or die(mysql_error());
 
-$id = "";
-while ($row = mysql_fetch_assoc($res)) {
-	$id = $row['id'];
-	break;
-}
+$id = mysql_insert_id();
 
 session_start();
 $_SESSION['uid'] = $id;
--- rm_main.php
+++ rm_main.php
@@ -31,6 +31,31 @@ if (!isset($_SESSION['uid'])) {
 
 
 <hr />
+
+<div id="content">
+<?php
+include_once("connect.php");
+$sql = "SELECT * FROM categories ORDER BY category_title ASC";
+$res = mysql_query($sql) or die(mysql_error());
+
+$categories = "";
+
+if (mysql_num_rows($res)>0) {
+	while ($row = mysql_fetch_assoc($res)) {
+		$id = $row['id'];
+		$title = $row['category_title'];
+		$description = $row['category_description'];
+		$categories .= "<a href='view_category.php?cid=".$id."' class='cat_links'>".$title." -<font size='-1'>".$description."</a>";
+	}
+
+	echo $categories;
+
+} else {
+	echo "<p>no category available yet\n</p>";
+}
+
+?>
+</div>
 </div>
 
 </body>
